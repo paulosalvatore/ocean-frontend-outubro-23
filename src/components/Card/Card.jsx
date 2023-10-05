7ece8c88b2519b08e5f2ed006b193f319a8dd5a8
Paulo Salvatore <salvatoregames@gmail.com>
2023-10-05 12:55:03 +0000

    Criação do componente `Card`

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+import './Card.css'
+
+export default function Card() {
+  return (
+    <div className="card">
+      <h2>Rick Sanchez</h2>
+      <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+    </div>
+  )
+}
