54be168d9150c6e8fbb996816163143de68bf9dd
Paulo Salvatore <salvatoregames@gmail.com>
2023-10-05 15:03:42 +0000

    Desafio 1 (HTML + CSS): resolvido

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,6 +6,13 @@ export default function Card(props) {
   return (
     <div className="card">
       <h2>{item.nome}</h2>
+
+      <div className="tags-list">
+        <div className="tag">Status: Vivo</div>
+        <div className="tag">Espécie: Humana</div>
+        <div className="tag">Origem: Terra C-137</div>
+      </div>
+
       <img src={item.imagemUrl} />
     </div>
   )
